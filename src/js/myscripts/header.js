0b40dcbf98d2ed94f4b20e009cf89a21d023b5a9
web-bombaster <skorpion_05@inbox.ru>
2023-04-10 16:05:23 +0300

    Страница услуг

--- src/js/myscripts/header.js
+++ src/js/myscripts/header.js
@@ -2,7 +2,6 @@ const headerFixed = () => {
     const header = document.querySelector('.header');
     const heightHeader = header.offsetHeight; // высота хедера
     let posTop = window.pageYOffset; // позиция прокрутки
-    console.log(posTop);
 
     if (posTop > 0) {
         header.classList.add('active');
@@ -14,6 +13,5 @@ const headerFixed = () => {
 };
 
 headerFixed();
-// запускаем headerFixed при ресайзе
 window.addEventListener("resize", headerFixed);
 window.addEventListener('scroll', headerFixed);
